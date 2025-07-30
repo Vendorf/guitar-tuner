6911fed44a32a15b770746e9c522a9534e5586ea
Victor Petrov <victor.petrov.cs@gmail.com>
2025-07-30 15:53:29 -0600

    Bear 🧸

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,31 @@ import { TuningProvider } from './context/TuningContext'
 import PitchDisplay from './components/PitchDisplay/PitchDisplay'
 import TunerDisplay from './components/TunerDisplay/TunerDisplay'
 import StartButton from './components/StartButton/StartButton'
+import bear from './assets/bear.png'
+// import scale from './assets/scale.m4a'
+import guitar1_stalker from './assets/guitar1_stalker.mp3'
+
+// Guitar from STALKER OST (https://archive.org/details/12.-alexey-omelchuk-call-of-pripyat-ost-outro/S.T.A.L.K.E.R/stalker_cs_ost_flac.mp3)
+// Zustand Bear from Zustand (https://zustand-demo.pmnd.rs/)
 
 function App() {
-  const [count, setCount] = useState(0)
+  //TODO: encase bear in proper div that scales correctly so that not stepping on top of content
+
+  //TODO: playing, setPlaying
+  //when click --> if playing, stop. if not, start
+  // also add multiple tracks and select a random one
+
+  //audio.pause()
+  //audio.currenTime = 0 to reset it
+  //
+  //audio.play() to start
+  //
+  //some event for when finishes to set to playing --> false
+
+  const audio = new Audio(guitar1_stalker)
+
+  // const [playAudio, stopPlayAudio]
+  // const [count, setCount] = useState(0)
 
   // return (
   //   <>
@@ -43,6 +65,7 @@ function App() {
     <>
       <AudioProvider>
         <TuningProvider>
+          <img src={bear} className="logo" alt="Zustand Bear" onClick={() => audio.play()}/>
           <StartButton></StartButton>
           <TunerDisplay></TunerDisplay>
           <PitchDisplay></PitchDisplay>
