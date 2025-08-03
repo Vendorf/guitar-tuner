91c0cc82837353db450b3880aab97849bd8239fa
Victor Petrov <victor.petrov.cs@gmail.com>
2025-08-02 18:07:13 -0600

    Add campfire animations 🏕️

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import PitchDisplay from './components/PitchDisplay/PitchDisplay'
 import TunerDisplay from './components/TunerDisplay/TunerDisplay'
 import StartButton from './components/StartButton/StartButton'
 import bear from './assets/bear.png'
+import campfire from './assets/campfire.png'
+import flame from './assets/flame.png'
+import soot from './assets/soot.png'
 // import scale from './assets/scale.m4a'
 import guitar1_stalker from './assets/guitar1_stalker.mp3'
 
@@ -27,6 +30,23 @@ function App() {
   //audio.play() to start
   //
   //some event for when finishes to set to playing --> false
+
+
+  const [darkMode, setDarkMode] = useState(false)
+  const [resetAnimation, setResetAnimation] = useState(false);
+
+  const toggleDark = () => {
+    setDarkMode(!darkMode)
+
+    if (darkMode) {
+      // Transitioning to light mode, so reset the smoke animation
+      setResetAnimation(true); // Remove animation class
+      setTimeout(() => {
+        setResetAnimation(false); // Add animation class back after a short delay
+      }, 10) // A small delay for reflow
+    }
+
+  }
 
   const audio = new Audio(guitar1_stalker)
 
@@ -73,7 +93,43 @@ function App() {
               <PitchDisplay></PitchDisplay>
             </div>
             <div className='sidebar sidebar-left'>
-              <img src={bear} className="bear" alt="Zustand Bear" onClick={() => audio.play()} />
+              <div className='camping-scene'>
+                <div className='camping-box'>
+                  <img src={bear} className="bear" alt="Zustand Bear" onClick={() => audio.play()} />
+                  <img src={campfire} className="campfire" alt="Campfire" onClick={() => toggleDark()}/>
+                  {/* <img src={flame} className='flame' style={{display: darkMode ? '' : 'none'}} alt='Flame' /> */}
+                  {/* <img src={flame} className={`flame ${darkMode ? '' : 'flame-off'}`} alt='Flame' /> */}
+                  {/* <img src={soot} className={`soot ${darkMode ? '' : ''}`} alt='Soot' style={{ display: darkMode ? 'none' : '' }} /> */}
+                  <img src={soot} className={`soot`} alt='Soot' />
+                  {/* <div className={`smoke-container ${darkMode ? 'smoke-off' : ''}`} style={{ display: darkMode ? 'none' : '' }}> */}
+                  {!resetAnimation && <div className={`smoke-container ${darkMode ? 'smoke-off' : ''}`} >
+                    <div className="smoke-particle sp0"></div>
+                    <div className="smoke-particle sp1"></div>
+                    <div className="smoke-particle sp2"></div>
+                    <div className="smoke-particle sp3"></div>
+                    <div className="smoke-particle sp4"></div>
+                  </div>}
+                  <div className={`flame-container ${darkMode ? '' : 'flame-off'}`}>
+                    <img src={flame} className={`flame ${darkMode ? '' : 'flame-off'}`} alt='Flame' />
+                  </div>
+
+
+                  {/* TODO: replace these with some sort of image ig */}
+                  {/* TODO: fire animation: 3 parts (orange, light orange, yellow) that sway back/forth; circle particles that come off and dissapear; black stroke around it? */}
+                  {/* <div class="smoke">
+                    <span class="s-0"></span>
+                    <span class="s-1"></span>
+                    <span class="s-2"></span>
+                    <span class="s-3"></span>
+                    <span class="s-4"></span>
+                    <span class="s-5"></span>
+                    <span class="s-6"></span>
+                    <span class="s-7"></span>
+                    <span class="s-8"></span>
+                    <span class="s-9"></span>
+                  </div> */}
+                </div>
+              </div>
             </div>
             <div className='sidebar sidebar-right'></div>
           </div>
