2b15f3cecaa03f0d079acd3d3c43828e14dea3e6
Victor Petrov <victor.petrov.cs@gmail.com>
2025-08-13 02:32:47 -0600

    useClickAway stale callback bugfix

--- src/hooks/useClickAway.jsx
+++ src/hooks/useClickAway.jsx
@@ -1,10 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
+
+// TODO: so this doesn't cause callbacks with stale state, can instead have this set some state/ref and then return that
+// then can useEffect() on the return of useClickAway in order to handle a click away event, after render, with latest state accessible
 
 const useClickAway = (containerRef, onClickOutside) => {
+    // Need to store this in a ref as otherwise caputed in initial closure so end up with stale state
+    const callbackRef = useRef(onClickOutside)
+
+    useEffect(() => {
+        // Update ref if callback ever changes
+        callbackRef.current = onClickOutside
+    }, [onClickOutside])
+
     useEffect(() => {
         const checkClickOutside = (e) => {
             if (containerRef.current && !containerRef.current.contains(e.target)) {
-                onClickOutside?.()
+                callbackRef.current?.()
             }
         }
 
