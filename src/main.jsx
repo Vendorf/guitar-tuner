2ec589ee65b344bb7815e37159a7c955bd9560fe
Victor Petrov <victor.petrov.cs@gmail.com>
2025-08-14 17:16:47 -0600

    Another windows style fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import useIsWindows from './hooks/useIsWindows.jsx'
 
 // import audioEle from './canvastest.jsx'
+
+const isWindows = useIsWindows()
+if (isWindows) {
+  document.documentElement.classList.add("on-windows")
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
